1366546b62c2bf7198af8580ac52b624ff09a6cf
petrichor1998 <parthpadalkar@gmail.com>
2021-01-14 16:25:53 -0600

    Create imp_functions.py

---
+++ imp_functions.py
@@ -0,0 +1,5 @@
+def convert_to_numerical(c_f_list, df):
+  for feature in cat_feature_list:
+    labels = df[feature].astype('category').cat.categories.tolist()
+    replace_dict = {feature : {k: v for k,v in zip(labels,list(range(1,len(labels)+1)))}}
+    df.replace(replace_dict, inplace=True)
