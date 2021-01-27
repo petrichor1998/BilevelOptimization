e06a4bb25d7ce1b224571636ec9a1d38fef930b9
petrichor1998 <parthpadalkar@gmail.com>
2021-01-26 23:24:24 -0600

    Update imp_functions.py

--- imp_functions.py
+++ imp_functions.py
@@ -18,9 +18,8 @@ def create_distributions(df, f_list):
 	return d_list
 
 #creating X_train and y_train for the full dataset
-def create_scaled_data(train, test):
+def create_scaled_data(train, test, scaler):
 	from sklearn.model_selection import train_test_split
-	from sklearn import preprocessing
 
 	X = train.iloc[:, :-1].values
 	y = train.iloc[:, -1].values
@@ -31,17 +30,15 @@ def create_scaled_data(train, test):
 	ytest = test.iloc[:, -1].values
 
 	#normalizing all values
-	min_max_scaler = preprocessing.MinMaxScaler()
-	train_scaled = min_max_scaler.fit_transform(X_train)
-	val_scaled = min_max_scaler.fit_transform(X_val)
-	test_scaled = min_max_scaler.fit_transform(X_test)
+	train_scaled = scaler.fit_transform(X_train)
+	val_scaled = scaler.transform(X_val)
+	test_scaled = scaler.transform(X_test)
 
 	return train_scaled, val_scaled, test_scaled, ytrain, yval, ytest
 
 #create distribution wise training and validation sets
-def create_distribution_train_val(dist_list):
+def create_distribution_train_val(dist_list, scaler):
 	from sklearn.model_selection import train_test_split
-	from sklearn import preprocessing
 
 	t_v_list = []
 	for d in dist_list:
@@ -49,13 +46,13 @@ def create_distribution_train_val(dist_list):
 		y = d.iloc[:, -1].values
 		Xd_train, Xd_val, yd_train, yd_val = train_test_split(X, y, test_size=0.2, random_state=1)
 
-		min_max_scaler = preprocessing.MinMaxScaler()
-		Xd_train_scaled = min_max_scaler.fit_transform(Xd_train)
-		Xd_val_scaled = min_max_scaler.fit_transform(Xd_val) 
+	Xd_train_scaled = scaler.fit_transform(Xd_train)
+		Xd_val_scaled = scaler.transform(Xd_val) 
 
 		t_v_list.append((Xd_train_scaled, Xd_val_scaled, yd_train, yd_val))
 
 	return t_v_list
+
 
 def calculate_total_accuracy(X, y, model):
 	with torch.no_grad():
@@ -68,12 +65,11 @@ def calculate_total_accuracy(X, y, model):
 		print("total number of items: {}".format(len(indices)))
 		print("Accuracy: {}".format(accuracy))
 
-def calculate_dist_accuracy(dist_list, model):
+def calculate_dist_accuracy(dist_list, model, scaler):
 	with torch.no_grad():
 		accuracy_list = []
-		min_max_scaler = preprocessing.MinMaxScaler()
 		for d in dist_list:
-			X_scaled = min_max_scaler.fit_transform(d.values[:, :-1])
+			X_scaled = scaler.fit_transform(d.values[:, :-1])
 			ypred_test = model(torch.Tensor(X_scaled))
 			values, indices = torch.max(ypred_test, 1)
 			correct = torch.eq(indices, torch.Tensor(d.values[:, -1])).sum()
